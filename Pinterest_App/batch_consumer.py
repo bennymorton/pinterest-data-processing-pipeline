9efc27a3d6104bcbac02c5ca32e80ba5c61af782
Ben <benjaminmorton@live.co.uk>
2023-01-27 15:10:35 +0000

    Milestone 7: Batch processing with Spark and orchestrating with Airflow

--- Pinterest_App/batch_consumer.py
+++ Pinterest_App/batch_consumer.py
@@ -5,8 +5,8 @@ import boto3
 
 consumer = KafkaConsumer(
     "pinterest-topic",
-    bootstrap_servers="localhost:9092"
-    # value_deserializer=lambda x: json.loads(x.decode("utf-8"))
+    bootstrap_servers="localhost:9092",
+    value_deserializer=lambda x: json.loads(x.decode("utf-8"))
 )
 
 admin_client = KafkaAdminClient(
@@ -17,6 +17,8 @@ admin_client = KafkaAdminClient(
 s3_client = boto3.client('s3')
 s3_resource = boto3.resource('s3')
 
-for msg in consumer:
-    object = s3_resource.Object('pinterest-data-d255ea73-811a-4331-98f6-7692459e2620', str(msg.timestamp))
-    s3_client.put_object(Body=msg.value, Bucket='pinterest-data-d255ea73-811a-4331-98f6-7692459e2620', Key=str(msg.timestamp))
+bucket = s3_resource.Bucket('pinterest-data-d255ea73-811a-4331-98f6-7692459e2620')
+
+for id, msg in enumerate(consumer):
+    filename = 'event' + str(id) + '.json'
+    bucket.put_object(Body=json.dumps(msg.value), Key=filename)
